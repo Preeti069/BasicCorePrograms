b177dfc4210c76850f65f2beecbc2a32b06606c1
preeti <pritich0896@gmail.com>
2023-02-08 20:01:11 +0530

    [Preeti]To Check given Year is Lear Year or Not

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LeapYear
+    {
+        public static void leapyear()
+        {
+            Console.WriteLine("Enter any year to check leap status");
+            int leap = Convert.ToInt32(Console.ReadLine());
+            if ((leap % 4 == 0) || (leap % 100 == 0))
+            {
+                Console.WriteLine($"{leap} is a Leap Year!");
+            }
+            else
+            {
+                Console.WriteLine($"{leap} is NOT a Leap Year!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -11,13 +11,17 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose any one program from below options");
-            Console.WriteLine("1:Flipcoin");
+            Console.WriteLine("1:Flipcoin \n2:Leap Year");
             int choose = Convert.ToInt32(Console.ReadLine());
             switch (choose)
             {
                 case 1:
                     Console.WriteLine("Flip Coin and print percentage of Heads and Tails :");
                     FlipCoin.flipcoin();
+                    break;
+                case 2:
+                    Console.WriteLine("To Check given Year is Lear Year or Not :");
+                    LeapYear.leapyear();
                     break;
                 default:
                     Console.WriteLine("Please enter number within range");
