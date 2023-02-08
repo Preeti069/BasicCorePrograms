0128146106296e18ef25c211c3b94ff33ba90166
preeti <pritich0896@gmail.com>
2023-02-08 19:34:00 +0530

    [Preeti]Flipping a Coin to check Head and Tail percentage

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose any one program from below options");
+            Console.WriteLine("1:Flipcoin");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    Console.WriteLine("Flip Coin and print percentage of Heads and Tails :");
+                    FlipCoin.flipcoin();
+                    break;
+                default:
+                    Console.WriteLine("Please enter number within range");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
